19d1415217c8056beb34329183c45f6229b93ea9
Eric Cestero <eric@cestero.io>
2022-03-24 14:09:35 -0400

    Ch 2: Updating the Todos list

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react"
 
 class InputTodo extends Component {
-  state = {
+
+    state = {
     title: "",
-  }
+  };
+
   onChange = e => {
     this.setState({
       [e.target.name]: e.target.value,
     })
-  }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addTodoProps(this.state.title);
+  };
+
   render() {
     return (
-      <form>
+        <form onSubmit={this.handleSubmit}>
         <input
           type="text"
           placeholder="Add todo..."
