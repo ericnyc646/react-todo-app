d634701d4a66c5e3cf42bfe4e4c2b97baba8d143
Eric Cestero <eric@cestero.io>
2022-03-24 13:32:38 -0400

    Ch 2 up to Using the setState updater

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -25,17 +25,20 @@ class TodoContainer extends React.Component {
        };
 
        handleChange = id => {
-        this.setState({
-          todos: this.state.todos.map(todo => {
-            if (todo.id === id) {
-              todo.completed = !todo.completed;
-            }
-            return todo;
-          })
-        });
+        this.setState(prevState => ({
+            todos: prevState.todos.map(todo => {
+              if (todo.id === id) {
+                return {
+                  ...todo,
+                  completed: !todo.completed,
+                }
+              }
+              return todo
+            }),
+          }));
       };
 
-      
+
     render() {
         return (
           <div>
